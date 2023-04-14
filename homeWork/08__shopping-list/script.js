74f59403fd5a3a97001cadf04f633873c932bbad
Oksana Melnyk <oks.melnuk@gmail.com>
2023-04-14 23:06:08 +0200

    styled checkbox

---
+++ homeWork/08__shopping-list/script.js
@@ -0,0 +1,13 @@
+// let form = document.querySelector('.form');
+// let field = document.querySelector('.list');
+// let checkboxes = form.querySelectorAll('input[type="checkbox"]');
+
+
+// field.addEventListener('click', () => {
+//   checkboxes.forEach(checkbox => {
+//     checkbox.checked = checkbox.checked ? false : true;
+//   })
+ 
+// }) 
+
+// // console.log('Go')
