dabd4974766e6de7a0e2063018b879f66e1a3133
Oksana Melnyk <oks.melnuk@gmail.com>
2023-04-17 11:46:38 +0200

    changed bem

--- homeWork/10_BEM/script.js
+++
@@ -1,7 +0,0 @@
-let burgerMenu = document.querySelector('#burger-menu');
-let overlay = document.querySelector('#menu')
-
-burgerMenu.addEventListener('click', () => {
-  burgerMenu.classList.toggle('header__burger-menu--close');
-  overlay.classList.toggle('overlay');
-});
