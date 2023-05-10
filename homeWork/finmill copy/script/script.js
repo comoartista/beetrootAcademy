229b1e9aff5e0c9c092984287033db390d39e7be
Oksana Melnyk <oks.melnuk@gmail.com>
2023-05-10 21:45:34 +0200

    added header, hero, about

---
+++ homeWork/finmill copy/script/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+  // slidesPerView: 1,
+  // spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+ 
+  },
+});
