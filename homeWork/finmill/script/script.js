8db6edd40d6c94c813069f1dadfca5d11ee9dc38
Oksana Melnyk <oks.melnuk@gmail.com>
2023-05-12 16:07:07 +0200

    added control-money

--- homeWork/finmill/script/script.js
+++ homeWork/finmill/script/script.js
@@ -1,21 +1,50 @@
+// var swiper = new Swiper(".mySwiper", {
+//   // slidesPerView: 1,
+//   // spaceBetween: 10,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
+
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+
+//     992: {
+//       slidesPerView: 3,
+//       spaceBetween: 32,
+//     },
+ 
+//   },
+// });
+
+
 var swiper = new Swiper(".mySwiper", {
-  // slidesPerView: 1,
-  // spaceBetween: 10,
+  slidesPerView: 1.5,
+  spaceBetween: 24,
   pagination: {
     el: ".swiper-pagination",
+    dynamicBullets: true,
     clickable: true,
   },
+
   breakpoints: {
-
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-
-    992: {
-      slidesPerView: 3,
-      spaceBetween: 32,
-    },
- 
+  576: {
+    slidesPerView: 1,
+    spaceBetween: 0,
   },
+
+  768: {
+    slidesPerView: 2,
+    spaceBetween: 20,
+  },
+
+  992: {
+    slidesPerView: 3,
+    spaceBetween: 32,
+  },
+
+},
 });
