39a80edda531f8053142cd0f384214aa1c208558
Oksana Melnyk <oks.melnuk@gmail.com>
2023-05-16 23:00:25 +0200

    added accordion

--- homeWork/finmill/script/script.js
+++ homeWork/finmill/script/script.js
@@ -99,17 +99,22 @@ var swiperPrevious = new Swiper(".mySwiper-previous", {
 },
 });
 
-const accordionItems = document.querySelectorAll('.accordion__item');
 
-accordionItems.forEach(item => {
-  const header = item.querySelector('.accordion__header');
-  const content = item.querySelector('.accordion__content');
-  const arrow = item.querySelector('.accordion__icon');
-
-
-  header.addEventListener('click', () => {
-    content.classList.toggle('active');
-    arrow.classList.toggle('active');
-
-  });
-});
+// Аккордеон
+function accordion() {
+  const items = document.querySelectorAll('.accordion__item-trigger')
+  items.forEach(item => {
+      item.addEventListener('click', () => {
+          const parent = item.parentNode
+          if (parent.classList.contains('accordion__item-active')) {
+              parent.classList.remove('accordion__item-active')
+          } else {
+              document
+                  .querySelectorAll('.accordion__item')
+                  .forEach(child => child.classList.remove('accordion__item-active'))   
+              parent.classList.add('accordion__item-active')
+          }
+      })
+  })
+}
+accordion() 
