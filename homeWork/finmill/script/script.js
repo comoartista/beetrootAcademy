759dbb875c4516ad0cfb79d16a23ed8fbd45431b
Oksana Melnyk <oks.melnuk@gmail.com>
2023-05-25 09:35:24 +0200

    fixed mobile

--- homeWork/finmill/script/script.js
+++ homeWork/finmill/script/script.js
@@ -1,4 +1,3 @@
-// Мобильное меню бургер
 function burgerMenu() {
   const burger = document.querySelector('.burger')
   const menu = document.querySelector('.menu')
@@ -13,6 +12,12 @@ function burgerMenu() {
           burger.classList.remove('active')
           body.classList.remove('locked')
       }
+  })
+
+  menu.addEventListener('click', () => {
+    menu.classList.remove('active')
+    burger.classList.remove('active')
+    body.classList.remove('locked')
   })
   // Вот тут мы ставим брейкпоинт навбара
   window.addEventListener('resize', () => {
@@ -100,7 +105,7 @@ var swiperPrevious = new Swiper(".mySwiper-previous", {
 });
 
 
-// Аккордеон
+// Accordion
 function accordion() {
   const items = document.querySelectorAll('.accordion__item-trigger')
   items.forEach(item => {
@@ -117,62 +122,34 @@ function accordion() {
       })
   })
 }
-accordion() 
+accordion();
 
 
-// Модальное окно
+// Modal
+function modal() {
+  const triggerModal = document.querySelectorAll('#modal__btn');
+  const modal = document.querySelector('.modal');
+  const closeModal = document.querySelector('.modal__close');
+  const body = document.body;
 
-const triggerModal = document.querySelectorAll('#modal__btn');
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.modal__close');
-const body = document.body;
+  triggerModal.forEach((item) => {
+    item.addEventListener('click', e => {
+      e.preventDefault();
+      modal.style.display = 'block';
+      body.classList.add('locked');
+    })
+  });
 
-triggerModal.forEach((item) => {
-  item.addEventListener('click', e => {
-    e.preventDefault();
-    modal.style.display = 'block';
-    body.classList.add('locked');
+  closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+    body.classList.remove('locked');
   })
-});
 
-closeModal.addEventListener('click', () => {
-  modal.style.display = 'none';
-  body.classList.remove('locked');
-})
-
-modal.addEventListener('click', e => {
-  if (e.target === modal) {
-    modal.style.display = 'none'
-    body.classList.remove('locked')
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      modal.style.display = 'none'
+      body.classList.remove('locked')
+    }
+  })
   }
-})
-// function bindModal(trigger, modal, close) {
-//   trigger = document.querySelector(trigger),
-//   modal = document.querySelector(modal),
-//   close = document.querySelector(close)
-
-//   const body = document.body
-
-//   trigger.addEventListener('click', e => {
-//     console.log('click');
-//     e.preventDefault()
-//     modal.style.display = 'block'
-//     body.classList.add('locked')
-//   });
-//   close.addEventListener('click', () => {
-//     modal.style.display = 'none'
-//      body.classList.remove('locked')
-//   });
-//   modal.addEventListener('click', e => {
-//     if (e.target === modal) {
-//       modal.style.display = 'none'
-//        body.classList.remove('locked')
-//     }
-//   })
-// }
-
-// // ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
-// // ВТОРОЙ аргумент - класс самого модального окна.
-// // ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
-// bindModal('#modal__btn', '.modal', '.modal__close')
-
+modal();
