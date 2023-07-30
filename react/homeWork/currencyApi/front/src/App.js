72600e56e03c275de72fe7c9ef4aacf9ede81b3c
Oksana Melnyk <oks.melnuk@gmail.com>
2023-07-30 22:37:36 +0200

    getted currancy values

--- react/homeWork/currencyApi/front/src/App.js
+++ react/homeWork/currencyApi/front/src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import useFetch from "react-fetch-hook";
+import {useState, useRef} from 'react'
 
 import "./App.css"
 
 
 /*
- 1: По кліку на Карточку на потрібно робити подію множення
+ 1: По кліку на Карточку потрібно робити подію множення
  (input.value * item.rate) і виводимо в такому форматі "0 €"
 
   2: Зробити клас ".active" на актуальній карточці
@@ -12,12 +14,15 @@ import "./App.css"
 
 export default function App() {
 
-    const {data, isLoading} = useFetch('https://www.binance.com/bapi/asset/v1/public/asset-service/product/currency')
+    const inp = useRef()
+    const [out, setOut] = useState(0)
 
+    const {data, isLoading} = useFetch('https://www.binance.com/bapi/asset/v1/public/asset-service/product/currency')
 
     console.log(data?.data)
 
     function Res(result) {
+      setOut((inp.current.value * result).toFixed(2))
         console.log(100 * result)
     }
 
@@ -33,8 +38,8 @@ export default function App() {
         <>
             <h1>Курс валют у USD</h1>
             <h2>Load</h2>
-            <input type="number"/>$
-            <span>Результат: 0 €</span>
+            <input type="number" ref={inp}/>$
+            <span>Результат: {out} €</span>
 
             <div className={"carts"}>
                 {List}
